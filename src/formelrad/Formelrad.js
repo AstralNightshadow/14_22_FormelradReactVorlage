f6cfbc529aaecfa0fe66ab6a2cd7c0b9f039ddfa
AstralNightshadow <phi.sutter@gmx.ch>
2025-05-22 11:17:17 +0200

    Calculation u and i is required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -14,6 +14,11 @@ export default function Formelrad() {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("handleSubmit")
+        if (values.u === "" && values.i === "") {
+            /*calculate u and i */
+            setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
+            setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
+        }
     }
 
     return (
@@ -25,7 +30,7 @@ export default function Formelrad() {
                 </header>
                 <form onSubmit={handleSubmit}>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
